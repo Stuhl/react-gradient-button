f8bea99d1487326a98f0a4cd3734ed3d23bd6276
Stuhl <BasicsDone7@web.de>
2019-03-30 13:56:09 +0100

    v1.1

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import React from "react"
-import ReactDOM from "react-dom"
-// import GradientButton from "./GradientButton"
-
-import styled from "styled-components"
-
-const Wrapper = styled.div`
-  button {
-    display: block;
-    margin: 50px;
-  }
-`
-
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <Wrapper>
-      <GradientButton text={"Your content"} gradientColor={{left: 250}} />
-      <GradientButton text={"Your content"} gradientColor={{left: 20}} />
-      <GradientButton text={"Your content"} gradientColor={{left: 100, mid: 150, right: 250}} />
-      <GradientButton text={"Your content"} borderRadius={10} gradientColor={{left: 280}} />
-    </Wrapper>, document.getElementById("app"))
-})
-
-// import GradientButton from "./GradientButton"
+// import React from "react"
+// import ReactDOM from "react-dom"
+// // import GradientButton from "./GradientButton"
 //
-// module.exports = {
-//   GradientButton
-// }
+// import styled from "styled-components"
+//
+// const Wrapper = styled.div`
+//   button {
+//     display: block;
+//     margin: 50px;
+//   }
+// `
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   ReactDOM.render(
+//     <Wrapper>
+//       <GradientButton text={"Your content"} gradientColor={{left: 250}} />
+//       <GradientButton text={"Your content"} gradientColor={{left: 20}} />
+//       <GradientButton text={"Your content"} gradientColor={{left: 100, mid: 150, right: 250}} />
+//       <GradientButton text={"Your content"} borderRadius={10} gradientColor={{left: 280}} />
+//     </Wrapper>, document.getElementById("app"))
+// })
+
+import GradientButton from "./GradientButton"
+
+module.exports = {
+  GradientButton
+}
